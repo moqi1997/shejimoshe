661c86380fb398704fb274a7588d519650e98dec
moqi <moqiwsmtec.com>
2019-11-29 14:10:34 +0800

    单例模式

---
+++ src/com/moqi/singleton/SweeptShop.java
@@ -0,0 +1,32 @@
+package com.moqi.singleton;
+
+public class SweeptShop {
+    public static void main(String[] args) {
+        System.out.println("inside main");
+        new Candy();
+        System.out.println("after creating");
+        try {
+            Class.forName("Gum");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Couldn't find Gum");
+        }
+        System.out.println("After Class.forName(\"Gum\")");
+        new Cookie();
+        System.out.println("After creating Cookie");
+    }
+}
+class Candy {
+    static {
+        System.out.println("Loading Candy");
+    }
+}
+class Gum {
+    static {
+        System.out.println("Loading Gum");
+    }
+}
+class Cookie {
+    static {
+        System.out.println("Loading Cookie");
+    }
+}
